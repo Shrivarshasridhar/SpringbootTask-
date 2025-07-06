748d742661d75a44a333c835eebb2452c38720e2
Shrivarsha <shrivarshak@gmail.com>
2025-07-06 18:56:03 +0530

    day2 and day3 research task added

---
+++ researchtask/researchtask/src/main/java/com/example/researchtask/ResearchtaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.researchtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchtaskApplication.class, args);
+	}
+
+}
---
+++ researchtask/researchtask/src/test/java/com/example/researchtask/ResearchtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.researchtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
