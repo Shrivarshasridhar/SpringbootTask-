74e079a3cec2a13cd7ed61a61f89081f68ee0cc2
Shrivarshasridhar <153291155+Shrivarshasridhar@users.noreply.github.com>
2025-07-14 21:25:29 +0530

    day 10

---
+++ employeespringboot/employeespringboot/src/test/java/com/example/employeespringboot/controllers/HelloTest.java
@@ -0,0 +1,15 @@
+package com.example.employeespringboot.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloTest {
+    @Test
+    public void tesHelloTest(){
+        Hello hell=new Hello();
+        String str= hell.helloTest();
+        System.out.println(str);
+        assertEquals("Hello Test",str);
+    }
+}
